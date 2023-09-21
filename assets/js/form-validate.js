16de89b685317bf7d914b25adec0348e41326c96
Mohsen Yazdandust <m.yazdandust1403@gmail.com>
2023-09-21 23:55:37 +0330

    booking

--- assets/js/form-validate.js
+++ assets/js/form-validate.js
@@ -6,9 +6,6 @@ function valid_datas( f ){
 	}else if( f.email.value == '' ){
 		jQuery('#form_status').html('<span class="wrong">Your email must not be empty and correct format!</span>');
 		notice( f.email );
-	//}else if( f.phone.value == '' ){
-		//jQuery('#form_status').html('<span class="wrong">Your phone must not be empty and correct format!</span>');
-		//notice( f.phone );
 	}else if( f.subject.value == '' ){
 		jQuery('#form_status').html('<span class="wrong">Your subject must not be empty!</span>');
 		notice( f.subject );
@@ -16,20 +13,33 @@ function valid_datas( f ){
 		jQuery('#form_status').html('<span class="wrong">Your message must not be empty!</span>');
 		notice( f.message );
 	}else{
-		 jQuery.ajax({
-			url: 'mail.php',
-			type: 'post',
-			data: jQuery('form#fruitkha-contact').serialize(),
-			complete: function(data) {
-				jQuery('#form_status').html(data.responseText);
-				jQuery('#fruitkha-contact').find('input,textarea').attr({value:''});
-				jQuery('#fruitkha-contact').css({opacity:1});
-				jQuery('#fruitkha-contact').remove();
-			}
-		});
-		jQuery('#form_status').html('<span class="loading">Sending your message...</span>');
-		jQuery('#fruitkha-contact').animate({opacity:0.3});
-		jQuery('#fruitkha-contact').find('input,textarea,button').css('border','none').attr({'disabled':''});
+		return true
+	}
+	
+	return false;
+}
+
+function valid_datas2( f ){
+	if( f.name.value == '' ){
+		jQuery('#form_status').html('<span class="wrong">Your name must not be empty!</span>');
+		notice( f.name );
+	}else if( f.email.value == '' ){
+		jQuery('#form_status').html('<span class="wrong">Your email must not be empty and correct format!</span>');
+		notice( f.email );
+	}else if( f.phonenumber.value == '' ){
+		jQuery('#form_status').html('<span class="wrong">Your phone must not be empty and correct format!</span>');
+		notice( f.phonenumber );
+	}else if( f.checkin.value == '' ){
+		jQuery('#form_status').html('<span class="wrong">Enter check in and check out dates!</span>');
+		notice( f.checkin );
+	}else if( f.checkout.value == '' ){
+		jQuery('#form_status').html('<span class="wrong">Enter check in and check out dates!</span>');
+		notice( f.checkout );
+	}else if( f.guests.value == '' ){
+		jQuery('#form_status').html('<span class="wrong">Enter number of guests!</span>');
+		notice( f.guests );
+	}else{
+		return true;
 	}
 	
 	return false;
